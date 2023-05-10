3856208013393429977f549e410fd1bfa57ce3c0
Stanislav22219 <128784563+Stanislav22219@users.noreply.github.com>
2023-05-10 12:03:10 +0300

    javascript

---
+++ QuickTask/index.js
@@ -0,0 +1,11 @@
+let a = '<h2>Hello, world!!!(console.log)</h2>'
+console.log(a)
+
+let b = '<h2>Hello, world!!!(document.write)</h2>'
+document.write(b)
+
+let c = prompt('Hello, world!!!');
+
+let d = confirm('Hello, world!!!');
+
+let e = alert('Hello, world!!!');
