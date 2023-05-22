a4aa62da2f33e2a2c0d3f745546cc2a4cfc49763
Stanislav22219 <128784563+Stanislav22219@users.noreply.github.com>
2023-05-22 23:38:46 +0300

    Add files via upload

---
+++ LabRab18/game.js
@@ -0,0 +1,55 @@
+'use strict'
+
+function game(){
+    let time = 60;
+    let countdown;
+    $('#left').html(time+' с');
+    $('#restart').prop('disabled', true);
+    countdown = setInterval(function(){
+        time--;
+        $('#left').html(time+' с');
+        if (time == 0){
+            alert('Час вийшов!');
+            clearInterval(countdown);
+            $('#restart').prop('disabled', false);
+        }
+    }, 1000);
+    
+    function shuffle(numbers) {
+        var ctr = numbers.length, temp, index;
+    
+        while (ctr > 0) {
+    
+            index = Math.floor(Math.random() * ctr);
+            ctr--;
+            temp = numbers[ctr];
+            numbers[ctr] = numbers[index];
+            numbers[index] = temp;
+        }
+        return numbers;
+    }
+
+    let numbers = Array.from({length: 25}, (_, i) => i + 1);
+    shuffle(numbers);
+    let n=0;
+    while (n<=25){
+        n++;
+        let font = Math.floor(Math.random() * 25)*1.9+10;
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        $('#'+n).html(numbers[n]);
+        $('#'+n).css('font-size', font+'pt');
+        $('#'+n).css('color', '#'+randomColor);
+        $('#'+n).click(check);
+        function check() {
+            if ($('#'+n).val==n){
+                console.log('s')
+            }
+        }
+    }
+    
+
+}
+
+$(document).ready(game)
+ 
+$('#restart').click(game)
--- LabRab18/script.js
+++ LabRab18/script.js
@@ -2,10 +2,4 @@
 
 $('#start').click(function() {
     window.location = "game.html";
-    let time = 60;
-    $('#left').html(time+' с');
  });
-
-
-
-
